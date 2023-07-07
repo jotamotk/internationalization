93c8f5d8b3d1258766987fdcb37863fdc3e65634
ToMaToJ <wangfangjie2012@hotmail.com>
2023-07-07 12:12:42 +0800

    [feat]action 中文翻译

--- lang 0.9/zh-CN/action.ts
+++ lang 0.9/zh-CN/action.ts
@@ -1,11 +1,11 @@
 export default {
   createSuccess: '创建成功',
-  editSuccess: 'Edit Success',
+  editSuccess: '编辑成功',
   deleteSuccess: '删除成功',
   renameSuccess: '重命名成功',
-  restoreSuccess: 'Restore Success',
-  moveSuccess: 'Move Success',
-  rerunSuccess: 'Rerun Success',
-  success: 'Success',
+  restoreSuccess: '恢复成功',
+  moveSuccess: '移动成功',
+  rerunSuccess: '重新运行成功',
+  success: '操作成功',
   // todo
 };
