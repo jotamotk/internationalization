4aee43f0f2d8f24c1dfabccc10851ab883d17990
ToMaToJ <wangfangjie2012@hotmail.com>
2023-07-17 10:00:43 +0800

    [refactor]remove duplicate zip

--- business/chatAi.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  datasetTips: 'Hover over the data and click "Annotate" to start!',
-  CopyInstruction: 'Copy and open instruction',
-  openTipsText:
-    "Hello! I'd be glad to assist you in setting up Ontologies. Please feel free to chat with me",
-};
--- business/recents.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  datasets: 'Datasets',
-  models: 'Models',
-  tasks: 'Tasks',
-  welcome: 'Welcome to BasicAI!',
-};
